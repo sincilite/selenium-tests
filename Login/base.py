1001bf709be772c1add861d0b3bef11843f8a121
Mike van Rooyen <mike@zaxity.com>
2017-03-09 13:12:17 +0000

    Refactor classes

--- Login/base.py
+++ Login/base.py
@@ -1,17 +1,30 @@
 import unittest
 from base import Base
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 class Login(Base):
 
-    """Base class for login tests
-
-    Attributes:
-        username    The username for a demo account
-        password    The pasword for a demo account
-
+    """ Base class for login tests
     """
 
-    username = "demo_user"
+    """ Request the login page
+    """
+    def launch_login(self, driver):
+        driver.get(Base.appSettings['domain'])
+        return driver
 
-    password = "demoadmin"
+    """ Populates the username and password fields with provided data
+    """
+    def provide_credentials(self, driver, username, password):
+        elem = driver.find_element_by_name("username")
+        elem.clear()
+        elem.send_keys(username)
+
+        elem = driver.find_element_by_name("password")
+        elem.clear()
+        elem.send_keys(password)
+        elem.send_keys(Keys.RETURN)
+
+        return driver
+
--- base.py
+++ base.py
@@ -1,20 +1,40 @@
 import unittest
+import sys
+import os
+import yaml
 import random
 import string
 from selenium import webdriver
 
 class Base(unittest.TestCase):
 
-    domain = 'http://ec2-52-212-152-82.eu-west-1.compute.amazonaws.com'
+    appSettings = {}
+
+    def loadConfig(self):
+        environment = os.environ["APPLICATION_ENV"];
+
+        with open("settings.yml", "r") as stream:
+            try:
+                config = yaml.load(stream)
+            except yaml.YAMLError as exc:
+                print (exc)
+
+            appConfig = {}
+            appConfig.update(config["global"])
+            appConfig.update(config[environment])
+            Base.appSettings = appConfig
+
+
+    def getAppSettings(self):
+        return self.appSettings
 
     def generate_random_string(size):
         return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(size))
 
     def setUp(self):
-        """Test
-        """
-        #self.driver = webdriver.Firefox()
-        self.driver = webdriver.PhantomJS()
+        self.loadConfig()
+        self.driver = webdriver.Firefox()
+        #self.driver = webdriver.PhantomJS()
 
     def tearDown(self):
         self.driver.quit()
